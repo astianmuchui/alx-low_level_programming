f094da54918c25b6ed7b12652ca61acfd206d82b
astianmuchui <sebastianmuchui79@gmail.com>
2022-12-19 08:53:29 +0300

    Switched to manjaro

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+int _atoi(char *s);
