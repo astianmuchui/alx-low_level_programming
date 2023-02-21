8a3fd8000723efed2a0a5d398578941dfbe3d406
astianmuchui <sebastianmuchui79@gmail.com>
2023-02-21 19:34:36 +0300

    Working on it

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,22 @@
 
 int print_sign(int n)
 {
+	int status;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		status = 1;
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		status = 0;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		status = -1;
 	}
+	return (status);
 }
