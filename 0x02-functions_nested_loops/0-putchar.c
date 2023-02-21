c107fdf8e2a578c34ce1bba666d04df308cd5e54
root <root@hunter>
2023-02-21 18:07:30 +0300

    Updates

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - prints _putchar then followed by a new line
+*Return: 0 if program executes
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
