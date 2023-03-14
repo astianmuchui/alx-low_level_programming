c243ca13f4563bfed6c7eb44e9e5025284484007
astianmuchui <sebastianmuchui79@gmail.com>
2023-03-14 18:34:28 +0300

    Malloc and Free

---
+++ OXOB-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* create_array - create array of size n and initiate it with character c
+* @size: the size of the desired array
+* @c: the character to initialize with
+* Return: NULL if size=0 ,  NULL if it fails ,
+* pointer to the array if there is sucess
+*/
+
+void *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else if (size  !=  0)
+	{
+		char arr[]  =  malloc((sizeof(char) * size));
+
+		if (s  ==  NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			arr[0]  =  c;
+
+			return (*arr);
+		}
+	}
+}
---
+++ OXOB-malloc_free/main.h
@@ -0,0 +1,2 @@
+int _putchar(char c);
+void *create_array(unsigned int size, char c);
