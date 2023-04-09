6283d16c335e9535219a36b796ed45ba4f66358d
astianmuchui <sebastianmuchui79@gmail.com>
2023-04-10 00:41:23 +0300

    minor fixes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (file_desc < 0)
 		return (-1);
 
-	if (text_content !== NULL)
+	if (text_content != NULL)
 	{
 		int strlen_t = strlen(text_content);
 
