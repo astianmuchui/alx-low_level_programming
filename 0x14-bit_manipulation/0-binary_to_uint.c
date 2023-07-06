e2400b919f3401e65340ac420304889392480239
astianmuchui <sebastianmuchui79@gmail.com>
2023-07-06 16:02:24 +0300

    Fixed compile errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,6 @@
 #include "main.h"
 
 /**
-* _exponent - raise a number to a power
-* @num: number to raise
-* @power: power to exponent with
-* Return: int answer
-*/
-
-int _exponent(int num, int power)
-{
-	int zero_power = 1;
-
-	int ans;
-
-	if (power == 0)
-		return (zero_power);
-	while (power > 0)
-	{	ans = 1 * num;
-
-		power--;
-	}
-	return (ans);
-}
-
-/**
 * binary_to_uint - converts a binary number to unsigned integer
 * @b: pointer to 0 and 1 chars
 * Return: converted number
@@ -32,23 +9,26 @@ int _exponent(int num, int power)
 
 unsigned int binary_to_uint(const char *b)
 {
-	int str_len, i;
-	unsigned int u_int = 0;
+        int i;
+        unsigned int result = 0;
+        int length = strlen(b);
 
-	if (b == NULL)
-		return (0);
+        for (i = 0; i < length; i++)
+        {
+                if (b[i] == '1')
+                {
+                        result = (result << 1) | 1;
+                 }
+                else if (b[i] == '0')
+                {
+                        result = result << 1;
+                }
+                else
+                {
+                        return 0;
+                }
+        }
 
-	str_len = strlen(b);
-
-
-	for (i = 0; i < str_len; i++)
-	{
-		if (b[i] != '0' && b[i] != '1') /* Handle invalid binary reps */
-			return (0);
-
-		u_int += (b[i] - '0') * _exponent(2, (str_len - (i + 1)));
-	}
-
-	return (u_int);
+    return result;
 }
 
