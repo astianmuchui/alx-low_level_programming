e3b1f6f52f42e36ae6ace120b8fddc8ea2e6d278
astianmuchui <sebastianmuchui79@gmail.com>
2023-07-06 13:14:37 +0300

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,23 +3,24 @@
 /**
 * print_binary - prints a number in binary form
 * @n: number to print
-* Return: void
+* Return: none
 */
 
 void print_binary(unsigned long int n)
 {
 	int j;
 
-	for (j = 1 << 31; j > 0; j = j / 2)
+	for (j = 31; j >= 0; j--)
 	{
-		if ((num & j) == 1)
+		if ((n & (1 << j)) != 0)
 		{
-			_putchar("1");
+			_putchar('1');
 		}
 		else
 		{
-			_putchar("0");
+			_putchar('0');
 		}
+
 	}
 	_putchar('\n');
 }
