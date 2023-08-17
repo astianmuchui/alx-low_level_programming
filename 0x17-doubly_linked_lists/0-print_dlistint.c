14c223dbfdc7d8636a210a59d0c1d32a13977fcd
astianmuchui <astianmuchui@gmail.com>
2023-08-17 10:06:52 +0300

    betty errors

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,7 +9,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t size = 0;
 
-	while (h)
+	if (!h)
+	{
+		return (NULL);
+	}
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,17 +3,22 @@
 /**
 * dlistint_len - returns the length of a linked list
 * @h: head node
-* Return: number of nodes 
+* Return: number of nodes
 */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t len = 0;
+	size_t len = 0;
 
-        while (h)
-        {
-                h = h->next;
-                len ++;
-        }
-        return (len);
+	if (!h)
+	{
+		return (NULL);
+	}
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
 }
