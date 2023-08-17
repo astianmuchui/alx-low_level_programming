c6723c0358738fd71b0986ac6a41ae6d10ffa080
astianmuchui <astianmuchui@gmail.com>
2023-08-17 10:29:29 +0300

    add node at the end

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -17,14 +17,15 @@
 
 typedef struct dlistint_s
 {
-        int n;
-        struct dlistint_s *prev;
-        struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif
