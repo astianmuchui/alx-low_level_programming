6b734e9799382db605255371fd78b8958c342492
astianmuchui <astianmuchui@gmail.com>
2023-08-17 09:28:02 +0300

    count the nodes of a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* dlistint_len - returns the length of a linked list
+* @h: head node
+* Return: number of nodes 
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t len = 0;
+
+        while (h)
+        {
+                h = h->next;
+                len ++;
+        }
+        return (len);
+}
