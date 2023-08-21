d12c3f115db14ff254d62e03fde98f36c3d5fd88
astianmuchui <astianmuchui@gmail.com>
2023-08-21 08:58:38 +0300

    fix betty checks

--- 0x18-dynamic_libraries/ctypes.c
+++ 0x18-dynamic_libraries/ctypes.c
@@ -4,7 +4,7 @@
 * add - adds two numbers
 * @a: first number
 * @b: second number
-* Return: sum of a and b 
+* Return: sum of a and b
 */
 
 int add(int a, int b)
--- 0x18-dynamic_libraries/ctypes.h
+++ 0x18-dynamic_libraries/ctypes.h
@@ -1,8 +1,10 @@
 #ifndef CTYPES_H
 #define CTYPES_H
+
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
 int div(int a, int b);
 int mod(int a, int b);
+
 #endif
