3ab522692f6bdb685dc82e0924532082d110a1ea
astianmuchui <astianmuchui@gmail.com>
2023-08-21 08:57:18 +0300

    C library to be called in python

---
+++ 0x18-dynamic_libraries/ctypes.c
@@ -0,0 +1,61 @@
+#include "ctypes.h"
+
+/**
+* add - adds two numbers
+* @a: first number
+* @b: second number
+* Return: sum of a and b 
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtracts two numbers
+* @a: first number
+* @b: second number
+* Return: difference of a and b
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplies two numbers
+* @a: first number
+* @b: second number
+* Return: product of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divides two numbers
+* @a: first number
+* @b: second number
+* Return: quotient of a and b
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod - finds modulus
+* @a: first number
+* @b: second number
+* Return: a modulo b
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
